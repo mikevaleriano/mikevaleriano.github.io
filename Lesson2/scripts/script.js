a87f5ec0e9f20a0047af99c1c84072e6ff378b8e
mikevaleriano <mvaleriano@byui.edu>
2020-09-26 17:05:06 -0500

    new folder and files for week2

---
+++ Lesson2/scripts/script.js
@@ -0,0 +1,6 @@
+
+let actualDate = new Date();
+let year = actualDate.getFullYear();
+let modified = document.lastModified;
+document.getElementById("lastUpdate").textContent = modified;
+document.getElementById("currentYear").textContent = year;
