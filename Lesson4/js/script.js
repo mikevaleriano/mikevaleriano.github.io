767807f6d1fa4c7fec228b529aa391cba674562a
mikevaleriano <mvaleriano@byui.edu>
2020-10-10 03:47:25 -0500

    font color change

--- Lesson4/js/script.js
+++ Lesson4/js/script.js
@@ -7,3 +7,19 @@ window.onresize = () => {
     if (window.innerWidth > 760)
         mainNav.classList.remove('responsive')
 };
+
+
+let actualDay = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+
+let actualMonth = [
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+
+let actualDate = new Date();
+let year = actualDate.getFullYear();
+
+
+document.getElementById("lastUpdate").textContent = actualDay[actualDate.getDay()] + ", " + actualDate.getDate() + " " + actualMonth[actualDate.getMonth()] + " " + actualDate.getFullYear();
+document.getElementById("currentYear").textContent = year;
