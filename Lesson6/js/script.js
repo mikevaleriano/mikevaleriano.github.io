44863fb5daf42c0faebd7307265ad82aa53f9338
mikevaleriano <mvaleriano@byui.edu>
2020-10-25 00:04:40 -0500

    .

---
+++ Lesson6/js/script.js
@@ -0,0 +1,29 @@
+const hamButton = document.querySelector('.ham');
+const mainNav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {mainNav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {
+    if (window.innerWidth > 760)
+        mainNav.classList.remove('responsive')
+};
+
+
+let actualDay = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+
+let actualMonth = [
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+
+let actualDate = new Date();
+let year = actualDate.getFullYear();
+
+
+document.getElementById("lastUpdate").textContent = actualDay[actualDate.getDay()] + ", " + actualDate.getDate() + " " + actualMonth[actualDate.getMonth()] + " " + actualDate.getFullYear();
+document.getElementById("currentYear").textContent = year;
+
+if (actualDate.getDate() == 7) {
+    document.getElementById('announcements').innerHTML= "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+}
